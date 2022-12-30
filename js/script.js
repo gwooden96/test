ea1a8980f2bdcc537dcca402d48c4d228f5c2915
kimjeonghan <gwooden22@gmail.com>
2022-12-30 18:15:14 +0900

    title 변경 및 제품카드  스크립트 통합 구축 작업

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+/* 반복 되는 것 배열로 통합 구축 작업
+
+let laptop = [
+  {name : '레노버 아이디어패드 Slim1-14ALC R3', os : '미포함(프리도스', displayInfo : '35.5cm(14인치)', displayLight : '220nit'}
+
+
+
+]
+
+
+laptop(())
+
+<div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">사무용 추천 노트북 No.01</li>
+  </ul>
+  <img src="img/레노버 노트북.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">레노버 아이디어패드 Slim1-14ALC R3</h5>
+    <p class="card-text">운영체제(OS): 미포함(프리도스) /  화면정보: 35.5cm(14인치) / 화면밝기: 220nit /  어댑터: 65W / 무게: 1.38kg</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">금액 : 399,000원(변동)</li>
+    <li class="list-group-item">프로세서: 라이젠 5300U (4코어)</li>
+    <li class="list-group-item">내장그래픽: Radeon Graphics</li>
+    <li class="list-group-item">메모리 용량: 8GB</li>
+    <li class="list-group-item"> 저장장치 M.2(NVMe) / 128GB</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">구매링크</a>
+    <a href="#" class="card-link">제품 상세 정보</a>
+  </div>
+*/
