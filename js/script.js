96fab4ff200f9db571327eb7f574898c7f9b983c
kimjeonghan <gwooden22@gmail.com>
2023-01-06 14:18:16 +0900

    header, nav, srch, login-btn, clock 추가 및 적용

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+function updateClock(){
+  let now = new Date();
+  let dname = hou = now.getHours(),
+      min = now.getMinutes(),
+      sec = now.getSeconds(),
+      pe = "AM";
+  
+      if(hou >= 12){
+        pe = "PM";
+      }
+      if(hou == 0){
+        hou = 12;
+      }
+      if(hou > 12){
+        hou = hou - 12;
+      }
+
+      
+      Number.prototype.pad = function(digits){
+        for(var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+      }
+
+ 
+
+      let ids = ["hour", "minutes", "seconds", "period"];
+      let values = [hou.pad(2), min.pad(2), sec.pad(2), pe];
+      for(var i = 0; i < ids.length; i++)
+      document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+}
+
+function initClock(){
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
+
+
+
 /* 반응형 꿀팁 그리드 저장변수 */
 let cardListWebNew = `<div class="accordion" id="accordionPanelsStayOpenExample">
 <div class="accordion-item">
